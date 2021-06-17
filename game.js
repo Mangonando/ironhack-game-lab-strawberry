4bfad972074feb23b3a55c254801985097e16fb6
Mangonando <juan.f.ramirezv@gmail.com>
2021-06-17 17:14:28 +0200

    second world done. lives and game over done. timer not done

--- game.js
+++ game.js
@@ -1,103 +1,136 @@
 class Game {
   constructor() {
     this.score = 0;
-    // this.timerValue = 0.001;
+    this.timerValue = 0;
     // this.startButton;
     // this.timeIt;
     // this.time = 0;
     // this.running = 1;
     // this.music;
+    this.lives = 3;
+    // this.millisecond = millis()
+    this.m = 0;
+    this.s = 0;
+    this.ms = 1;
   }
 
   setup() {
+    this.world1 = new World(30, 20, 350, 150);
+    this.player1 = new Player(50, 40, -0.5);
+    this.world2 = new World(350, 250, 500, 250);
+    this.player2 = new Player(400, 300, 0.5);
     // this.music = this.music = loadSound('POL-mad-run-short.wav', loaded)
-    this.player = new Player(0, 0);
-    this.world = new World(30, 20, 250, 250);
-    // setInterval(this.timeIt, 100);
-    // this.increment();
-    // this.player2 = new Player(300, 300);
-    // this.world2 = new World(300, 300, 250, 250);
+    setInterval(this.timer,)
   }
 
+  timer() {
+    this.ms++;
+    if (this.ms % 1000 === 0) {
+      this.ms = 0;
+      this.s++;
+    }
+  }
+  // this.ms = millis();
+  // textSize(32);
+  // fill("black");
+  // noStroke();
+  // text("Milliseconds \nrunning: \n" + this.ms, 5, 40);
+
   draw() {
+    this.timerValue;
+    this.timer();
     //   fullscreen(true)
-    // console.log("players", this.player1, this.player2);
-    this.player.draw();
-    this.world.draw();
-    // this.player2.draw();
-    // this.world2.draw();
-    switch (this.world.color) {
+
+    clear();
+    this.player1.draw();
+    this.world1.draw();
+    this.player2.draw();
+    this.world2.draw();
+
+    switch (this.world1.color) {
       case BLUE:
-        this.player.teleport();
+        this.player1.teleport(this.world1);
         break;
       case ORANGE:
-        this.player.wall();
+        this.player1.wall(this.world1);
         break;
     }
-//     if (this.timerValue >= 10) {
-//       text("00:" + this.timerValue, width / 2, height / 2);
-//     }
-//     if (this.timerValue < 10) {
-//       text("00.0" + this.timerValue, width / 2, height / 2);
-//     }
-   }
 
-//   timeIt() {
-//     if (this.timerValue > 0) {
-//       this.timerValue++;
-//     }
-//   }
-}
-  //   increment() {
-  //       console.log("increment", this.running)
-  //     if (this.running == 1) {
-  //       setTimeout(function () {
-  //         this.time++;
-  //         let mins = Math.floor(this.time / 10 / 60);
-  //         let secs = Math.floor((this.time / 10) % 60);
-  //         let tenths = this.time % 10;
-  //         if (mins < 10) {
-  //           mins = "0" + mins;
-  //         }
-  //         if (secs < 10) {
-  //           secs = "0" + secs;
-  //         }
-  //         // document.getElementById("output").innerHTML =
-  //         //   mins + ":" + secs + ":" + "0" + tenths;
-  //         console.log("time", mins + ":" + secs + ":" + "0" + tenths)
-  //         this.increment();
-  //       }, 100);
+    switch (this.world2.color) {
+      case BLUE:
+        this.player2.teleport(this.world2);
+        break;
+      case ORANGE:
+        this.player2.wall(this.world2);
+        break;
+    }
+    
+    // this.timerValue++;
+    textSize(32);
+    fill("black");
+    noStroke();
+    text(this.s + ` : ` + this.ms, 200, 150);
+    //     if (this.timerValue >= 10) {
+    // text("00:" + this.timerValue, width / 2, height / 2);
+    //     }
+    //     if (this.timerValue < 10) {
+    //       text("00.0" + this.timerValue, width / 2, height / 2);
+    //     }
+    // let millisecond = millis();
+    // textSize(32);
+    // fill('black');
+    // noStroke();
+    // text("00:" + millisecond, 500, 150);
+    // textSize(32);
+    // fill("black");
+    // noStroke();
+
+    // text(this.m, 500, 150);
+    // text(this.s, 550, 150);
+    // text(this.ms, 600, 150);
+    // console.log(`${this.s}:${this.ms}`)
+  }
+
+  //   millis() {
+  //     text('Milliseconds \nrunning: \n' + millisecond, 5, 40);
+  //   }
+
+  livesGone() {
+    let currentLives = document.getElementById("life-" + this.lives);
+    console.log("actual live", currentLives);
+    if (currentLives) {
+      currentLives.style.backgroundColor = ORANGE;
+      this.lives--;
+    }
+    if (this.lives === 0) {
+      let gameOver = document.getElementById("game-over");
+      gameOver.style.opacity = 10;
+    }
+  }
+
+  //   timeIt() {
+  //     if (this.timerValue > 0) {
+  //       this.timerValue++;
   //     }
-
-// if (frameCount % 100 === 0) {
-//   if (this.color === BLUE) {
-//     this.color = ORANGE;
-//   } else if (this.color === ORANGE) {
-//     this.color = BLUE;
-//   }
-// }
-
-//    draw() {
-//     //   fullscreen(true)
-//     console.log("players", this.player1, this.player2)
-//     this.player1.draw();
-//     this.world1.draw();
-//     switch (this.world1.color) {
-//       case BLUE:
-//         this.player1.teleport();
-//         break;
-//       case ORANGE:
-//         this.player1.wall();
-//         break;
+  //   }
+}
+//   increment() {
+//       console.log("increment", this.running)
+//     if (this.running == 1) {
+//       setTimeout(function () {
+//         this.time++;
+//         let mins = Math.floor(this.time / 10 / 60);
+//         let secs = Math.floor((this.time / 10) % 60);
+//         let tenths = this.time % 10;
+//         if (mins < 10) {
+//           mins = "0" + mins;
+//         }
+//         if (secs < 10) {
+//           secs = "0" + secs;
+//         }
+//         // document.getElementById("output").innerHTML =
+//         //   mins + ":" + secs + ":" + "0" + tenths;
+//         console.log("time", mins + ":" + secs + ":" + "0" + tenths)
+//         this.increment();
+//       }, 100);
 //     }
-//     this.player2.draw();
-//     this.world2.draw();
-//     switch (this.world2.color) {
-//       case BLUE:
-//         this.player2.teleport();
-//         break;
-//       case ORANGE:
-//         this.player2.wall();
-//         break;
-//     }
-//    }
--- player.js
+++ player.js
@@ -1,74 +1,62 @@
 class Player {
-  constructor(x, y) {
-    this.x = Math.floor(Math.random() * 200) + 50;
-    this.y = Math.floor(Math.random() * 200) + 50;
+  constructor(x, y, gravity) {
+    this.x = world.x + world.width / 2 //Math.floor(Math.random() * 200) + 50;
+    this.y = world.y + world.height / 2 //Math.floor(Math.random() * 200) + 50;
     this.diameter = 25;
     // this.number;
     // this.speed = 0;
-    this.gravity = - 1.5;
-    this.lives = 3;
+    this.gravity = gravity;
+    
+  }
+  teleport(world) {
+    if (this.x > world.x + world.width - 0.5 * this.diameter) {
+      this.x = world.x + 0.5 * this.diameter;
+    }
+    if (this.x < world.x + 0.5 * this.diameter) {
+      this.x = world.x + world.width - 0.5 * this.diameter;
+    }
+    if (this.y > world.y + world.height - 0.5 * this.diameter) {
+      this.y = world.y + 0.5 * this.diameter;
+    }
+    if (this.y < world.y + 0.5 * this.diameter) {
+      this.y = world.y + world.height - 0.5 * this.diameter;
+    }
   }
 
-  teleport() {
-    if (this.x > game.world.x + game.world.width - 0.5 * this.diameter) {
+  wall(world) {
+    // console.log("wall");
+    if (this.x > world.x + world.width - 0.5 * this.diameter) {
       console.log("right squaaaaare");
-      this.x = game.world.x + 0.5 * this.diameter;
+      this.x = world.x + world.width / 2;
+      this.y = world.y + world.height / 2;
+      game.livesGone();
     }
-    if (this.x < game.world.x + 0.5 * this.diameter) {
+    if (this.x < world.x + 0.5 * this.diameter) {
       console.log("left squaaaaare");
-      this.x = game.world.x + game.world.width - 0.5 * this.diameter;
+      this.x = world.x + world.width / 2;
+      this.y = world.y + world.height / 2;
+      game.livesGone();
     }
-    if (this.y > game.world.y + game.world.height - 0.5 * this.diameter) {
+    if (this.y > world.y + world.height - 0.5 * this.diameter) {
       console.log("down squaaaaare");
-      this.y = game.world.y + 0.5 * this.diameter;
+      this.x = world.x + world.width / 2;
+      this.y = world.y + world.height / 2;
+      game.livesGone();
     }
-    if (this.y < game.world.y + 0.5 * this.diameter) {
+    if (this.y < world.y + 0.5 * this.diameter) {
       console.log("up squaaaaare");
-      this.y = game.world.y + game.world.height - 0.5 * this.diameter;
+      this.x = world.x + world.width / 2;
+      this.y = world.y + world.height / 2;
+      game.livesGone();
     }
-  }
-  wall() {
-    if (this.x > game.world.x + game.world.width - 0.5 * this.diameter) {
-      this.x = game.world.x + (game.world.width / 2);
-      this.y = game.world.y + (game.world.height / 2);
-      this.livesGone()
-    }
-    if (this.x < game.world.x + 0.5 * this.diameter) {
-      console.log("left squaaaaare");
-      this.x = game.world.x + (game.world.width / 2);
-      this.y = game.world.y + (game.world.height / 2);
-
-      this.livesGone()
-    }
-    if (this.y > game.world.y + game.world.height - 0.5 * this.diameter) {
-      console.log("down squaaaaare");
-      this.x = game.world.x + (game.world.width / 2);
-      this.y = game.world.y + (game.world.height / 2);
-      this.livesGone()
-    }
-    if (this.y < game.world.y + 0.5 * this.diameter) {
-      console.log("up squaaaaare");
-      this.x = game.world.x + (game.world.width / 2);
-      this.y = game.world.y + (game.world.height / 2);
-      this.livesGone()
-    }
-  }
-
-  livesGone() {
-      this.lives --
-    if (this.lives === 0) {
-        console.log("Game Over");
-      }
   }
 
   draw() {
-    clear();
     fill(BLUE);
     stroke(BLUE);
     ellipse(this.x, this.y, this.diameter, this.diameter);
     this.y = this.y + this.gravity;
     // this.speed = this.speed + this.gravity;
-
 
     if (keyIsDown(LEFT_ARROW)) {
       this.x -= 5;
