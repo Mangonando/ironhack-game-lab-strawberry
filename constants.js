f9c5e81193de8a400c4767398d99789c28bea8e1
Mangonando <juan.f.ramirezv@gmail.com>
2021-08-10 18:56:01 +0200

    cleaning code

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 const WIDTH = window.screen.availWidth - 200;
 const HEIGHT = window.screen.availHeight - 200;
-const BLUE = 'rgb(51, 236, 255)';
-const ORANGE = 'rgb(255, 104, 0)';
+const BLUE = "rgb(51, 236, 255)";
+const ORANGE = "rgb(255, 104, 0)";
